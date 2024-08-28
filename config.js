75873a655d56b4ae21a3df273edda700d3f64ce0
mrtaz77 <mrtazwar23@gmail.com>
2024-08-28 16:46:22 +0600

    update config

--- config.js
+++ config.js
@@ -20,5 +20,8 @@ export const clientConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
