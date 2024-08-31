80fbbfc6449922a205065b4ead0e6c9cac35dce6
mrtaz77 <mrtazwar23@gmail.com>
2024-08-31 08:20:27 +0600

    working text field

--- app/login/page.js
+++ app/login/page.js
@@ -44,28 +44,72 @@ export default function Register() {
 				</Typography>
 
 				<TextField
-					label=""
+					label="Enter your email"
 					variant="outlined"
 					fullWidth
 					margin="normal"
 					type="email"
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
-					className={styles.textField}
+					sx={{
+						marginBottom: '1.5rem',
+						backgroundColor: '#2c2c2c',
+						borderColor: '#444',
+						'& .MuiInputBase-input': {
+							color: '#ffffff',
+						},
+						'& .MuiInputLabel-root': {
+							color: '#e0e0e0',
+						},
+						'& .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#444',
+						},
+						'&:hover .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#00BF56',
+						},
+						'& .MuiInputLabel-root.Mui-focused': {
+							color: '#00BF56',
+						},
+						'& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#00BF56',
+						},
+					}}
 				/>
 				<Typography variant="body1" align="left">
 					Your Password
 				</Typography>
 
 				<TextField
-					label=""
+					label="Enter your password"
 					variant="outlined"
 					fullWidth
 					margin="normal"
 					type="password"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
-					className={styles.textField}
+					sx={{
+						marginBottom: '1.5rem',
+						backgroundColor: '#2c2c2c',
+						borderColor: '#444',
+						'& .MuiInputBase-input': {
+							color: '#ffffff',
+						},
+						'& .MuiInputLabel-root': {
+							color: '#e0e0e0',
+						},
+						'& .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#444',
+						},
+						'&:hover .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#00BF56',
+						},
+						'& .MuiInputLabel-root.Mui-focused': {
+							color: '#00BF56',
+						},
+						'& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+							borderColor: '#00BF56',
+						},
+					}}
 				/>
 
 				<Button
